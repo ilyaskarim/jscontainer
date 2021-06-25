d45c60a69dda037bc7e3c7e77c5e347a0827a582
ilyas <ilyas.datoo@gmail.com>
2021-06-25 21:59:07 +0500

    Update Assets.tsx

--- components/Container/Assets.tsx
+++ components/Container/Assets.tsx
@@ -10,38 +10,26 @@ export default function () {
   const handlerSubmit = (e: any) => {
     console.log("submit");
     e.preventDefault();
-  }
-    return (
+  };
+  return (
+    <>
       <div className="url_box">
         <a className="url_link" href="#">
           https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
         </a>
         <div className="url_icons">
-          <span  onClick={ () => {
-            toast.success("Removed successfully")
-          } } >
+          <span
+            onClick={() => {
+              toast.success("Removed");
+            }}
+          >
             <i className="fas fa-times"></i>
           </span>
           <span onClick={() => setOpen(true)}>
             <i className="fas fa-plus"></i>
           </span>
         </div>
-        <Modal  className="assets-modal invite-modal " isOpen={open} onRequestClose={() => setOpen(false)}>
-            <div className="modal-header pt-0 pl-0 border-0">
-              <a href="" className="mr-3 ">CDN JS</a>
-              <a href="">Link</a>
-            </div>
-            <div className="assets-content invite-content">
-              <form action="" onSubmit={handlerSubmit} >
-                <InputField placeholder="Search a library from cdnjs" className=""/>
-              </form>
-              <p className="mb-3">Tip: To directly insert, enter url and press enter</p>
-              <div className="cdn-links mb-2"><a href="">https://wapgee.com/cdn/path/to/file.js</a> <span>Vue @ 2.5.6</span></div>
-              <div className="cdn-links"><a href="">https://wapgee.com/cdn/path/to/file.js</a> <span>Vue @ 2.5.6</span></div>
-            </div>
-          </div>
-        );
-      })}
+      </div>
       <Modal
         className="assets-modal invite-modal "
         isOpen={open}
@@ -54,18 +42,26 @@ export default function () {
           <a href="">Link</a>
         </div>
         <div className="assets-content invite-content">
-          <InputField placeholder="Search a library from cdnjs" className="" />
-          <p className="mb-3">Invite to your container</p>
-          <p className="mb-4">
-            Inviting users to your container only works <br /> when you have a
-            private container
-          </p>
           <form action="" onSubmit={handlerSubmit}>
-            <Button className="btn btn-primary btn-xs">Invite</Button>
+            <InputField
+              placeholder="Search a library from cdnjs"
+              className=""
+            />
           </form>
+          <p className="mb-3">
+            Tip: To directly insert, enter url and press enter
+          </p>
+          <div className="cdn-links mb-2">
+            <a href="">https://wapgee.com/cdn/path/to/file.js</a>{" "}
+            <span>Vue @ 2.5.6</span>
+          </div>
+          <div className="cdn-links">
+            <a href="">https://wapgee.com/cdn/path/to/file.js</a>{" "}
+            <span>Vue @ 2.5.6</span>
+          </div>
         </div>
       </Modal>
-      <Toaster />
+      <Toaster></Toaster>
     </>
   );
 }
