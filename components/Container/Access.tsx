ceed44f0b6faf96853b7d605641b4c0e017a8960
ilyas <ilyas.datoo@gmail.com>
2021-06-25 22:12:15 +0500

    Update Access.tsx

--- components/Container/Access.tsx
+++ components/Container/Access.tsx
@@ -2,43 +2,62 @@ import Button from "./../UI/Button";
 import Modal from "../../components/UI/InviteModal";
 import InputField from "../../components/UI/InputField";
 import { useState } from "react";
-
+import toast, { Toaster } from "react-hot-toast";
 
 export default function () {
   const [open, setOpen] = useState(false);
 
   const handlerSubmit = (e: any) => {
-    console.log("submit")
+    console.log("submit");
     e.preventDefault();
-  }
+  };
 
-
-    return (
+  return (
+    <>
       <div className="access scroll-bar">
         <div className="scroll-bar">
           {[1, 2, 3, 4, 5].map((c) => {
             return (
               <div className="d-inline-block p-10 mr-2 mb-2 pr-0 invited-user">
                 ilyas@gmail.com
-                <span className="close d-inline-block ml-1 text-sm">
+                <span
+                  className="close d-inline-block ml-1 text-sm"
+                  onClick={() => {
+                    toast.success("User removed");
+                  }}
+                >
                   &times;
                 </span>
               </div>
             );
           })}
-          <Button onClick={() => setOpen(true)} className="btn btn-primary btn-xs">Invite</Button>
-          
-          <Modal  className="invite-modal" isOpen={open} onRequestClose={() => setOpen(false)}>
-            <div className="invite-content">
-              <h4 className="mb-3">Invite to your container</h4>
-              <p className="mb-4">Inviting users to your container only works <br /> when you have a private container</p>
-              <form action="" onSubmit={handlerSubmit} >
-                <InputField placeholder="Enter email address" className="mb-4"/> <br />
-                <Button className="btn btn-primary btn-xs">Invite</Button>
-              </form>
-            </div>
-          </Modal>
+          <Button
+            onClick={() => setOpen(true)}
+            className="btn btn-primary btn-xs"
+          >
+            Invite
+          </Button>
         </div>
       </div>
-    );
+      <Modal
+        className="invite-modal"
+        isOpen={open}
+        onRequestClose={() => setOpen(false)}
+      >
+        <div className="invite-content">
+          <h4 className="mb-3">Invite to your container</h4>
+          <p className="mb-4">
+            Inviting users to your container only works <br /> when you have a
+            private container
+          </p>
+          <form action="" onSubmit={handlerSubmit}>
+            <InputField placeholder="Enter email address" className="mb-4" />{" "}
+            <br />
+            <Button className="btn btn-primary btn-xs">Invite</Button>
+          </form>
+        </div>
+      </Modal>
+      <Toaster></Toaster>
+    </>
+  );
 }
