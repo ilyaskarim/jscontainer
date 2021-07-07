a6fc109fff74cadf612ed52109346630607c7d23
shahab <shahabkhan56121@gmail.com>
2021-07-07 11:12:05 +0500

    changes

--- components/Navbar/User.tsx
+++ components/Navbar/User.tsx
@@ -1,7 +1,34 @@
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import Link from "next/link";
+import { useState } from "react";
+
+
 
 export default function () {
+  const [isLoggedIn, setIsLoggedIn]=useState(false);
+  const userlogin = (props: any) => {
+    return (
+      <>
+        <Link href="/profile">Signup </Link>
+        <Link href="/profile">Login </Link>
+      
+      </>
+    )
+  }
+  const userlogOut= (props: any) => {
+    return (
+      <>
+        <Link href="/profile"><img src="" alt="" /> <>
+          <h5>Ilyas Karim</h5>
+          <span>@iamdattoo</span>
+        </></Link>
+        <Link href="/profile">Profile </Link>
+        <Link href="/profile">Containers </Link>
+        <Link href="/profile" >Logout </Link>
+      </>
+    )
+  }
+  
   return (
     <Menu
       className=""
@@ -12,8 +39,9 @@ export default function () {
       }
     >
       <MenuItem className="">
+        {isLoggedIn ? userlogin : userlogOut}
         <Link href="/profile">Profile </Link>
       </MenuItem>
-    </Menu>
+    </Menu> 
   );
 }
