5c3c0b97c4d90a6568a773f26c9792101863bfde
ilyas <ilyas.datoo@gmail.com>
2021-07-09 16:32:39 +0500

    Changed particles.

--- pages/playgrounds.tsx
+++ pages/playgrounds.tsx
@@ -1,53 +1,85 @@
+import { useRef } from "react";
 import { useEffect } from "react";
 import PlaygroundItem from "../components/UI/playgroundItem";
 
 export default function () {
   useEffect(() => {
-    let ball = document.createElement("div");
-    let dd = document.querySelector(".bubbles")
-    // Some random colors
-    const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+    if (process.browser) {
+      const els: Array<HTMLDivElement> = []
+      const styleDiv = (div: HTMLDivElement) => {
+        const random = Math.floor(Math.random() * 2);
+        switch (random) {
+          case 0:
+            const res = Math.floor(Math.random() * 15);
+            div.style.height = `${res}px`;
+            div.style.width = `${res}px`;
+            div.style.borderRadius = "50%";
+            break;
+          case 1:
+            const width = Math.floor(Math.random() * 10);
+            div.style.height = `3px`;
+            div.style.width = `${width}px`;
+            break;
 
-    const numBalls = 50;
-    const balls = [];
-
-    for (let i = 0; i < numBalls; i++) {
-      let ball = document.createElement("div");
-      ball.classList.add("ball");
-      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-      ball.style.transform = `scale(${Math.random()})`;
-      ball.style.width = `${Math.random()}em`;
-      ball.style.height = ball.style.width;
-
-      balls.push(ball);
-      dd?.append(ball)
-    }
-
-    // Keyframes
-    balls.forEach((el, i, ra) => {
-      let to = {
-        x: Math.random() * (i % 2 === 0 ? -10 : 120),
-        y: Math.random() * 3,
-      };
-
-      let anim = el.animate(
-        [
-          { transform: "translate(40rem)" },
-          { animation: "fadeIn ease 5s"},
-
-          { transform: `translate(${to.x}rem, ${to.y}rem)` },
-        ],
-        {
-          duration: (Math.random() + 1) * 3000, // random duration
-          direction: "alternate",
-          fill: "both",
-          iterations: Infinity,
-          easing: "ease-in-out",
+          default:
+            break;
         }
-      );
-    });
+      };
+      const bubblesEL = document.querySelector(".bubbles");
+      const mainEl = document.querySelector(".playgrounds_image");
+      const colors = [
+        "rgb(255 0 0)",
+        "rgb(0 102 255)",
+        "rgb(255, 235, 59)",
+        "rgb(27 0 255)",
+        "rgb(247 0 0)",
+      ];
+
+      const totalItems = 90;
+      for (let index = 0; index < totalItems; index++) {
+        const el = document.createElement("div");
+        styleDiv(el);
+        el.style.position = "absolute";
+        el.style.background = colors[Math.floor(Math.random() * colors.length)];
+        el.style.left =
+          Math.floor(Math.random() * (mainEl?.clientWidth || 0) + 300) + "px";
+        el.style.top =
+          Math.floor(Math.random() * (mainEl?.clientHeight || 0) - 100) + "px";
+        bubblesEL?.append(el);
+        els.push(el);
+      }
+      els.forEach((el, i) => {
+        let to = {
+          x: Math.random() * (i % 2 === 0 ? -11 : 11),
+          y: Math.random() * 12,
+        };
+
+        el.animate(
+          [
+            { transform: "translate(0, 0)" },
+            { transform: `translate(${to.x}rem, ${to.y}rem)` },
+          ],
+          {
+            duration: (Math.random() + 1) * 2000, // random duration
+            direction: "alternate",
+            fill: "both",
+            iterations: Infinity,
+            easing: "ease-in-out",
+          }
+        );
+      });
+      var oldx = 0;
+
+      // document.addEventListener("mousemove", function (e) {
+      //   if (e.pageX < oldx) {
+      //     console.log("left");
+      //   } else if (e.pageX > oldx) {
+      //     console.log("right");
+      //   }
+
+      //   oldx = e.pageX;
+      // });
+    }
   });
 
   const cardsData = [
@@ -58,26 +90,22 @@ export default function () {
     {
       title: "Slugify",
       para: "Convert any string into slugify. Slugify is used to convert strings into readable URL’s.",
-    }
-  ]
-
+    },
+  ];
 
   return (
     <div className="playgrounds_section">
       <div className="playgrounds_image">
         <div className="bubbles"></div>
         <div className="content">
-            <h1>Playgrounds</h1>
-            <p>Playgrounds to ehance your productivty:</p>
+          <h1>Playgrounds</h1>
+          <p>Playgrounds to ehance your productivty:</p>
         </div>
       </div>
       <div className="cards-section">
         {cardsData.map((item) => {
-          return(
-            <PlaygroundItem item={item}/>
-            )
-          })
-        }
+          return <PlaygroundItem item={item} />;
+        })}
         <div className="loadMore">
           <a href="#">More playgounds coming soon...</a>
         </div>
