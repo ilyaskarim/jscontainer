87e26a226f41f414124091135e7f3459ff1b4e24
ilyas <ilyas.datoo@gmail.com>
2021-07-16 03:40:19 +0500

    test

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -11,7 +11,7 @@ export default function () {
         <div className="bubbles"></div>
         <div className="content">
           <h1>Blog</h1>
-          <p>Playgrounds to ehance your productivty:</p>
+          <p>Find the best blogs, articles and tutorials about frontend development from all over the web.</p>
         </div>
       </div>
     </div>
