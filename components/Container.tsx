ca8a573d99d0ca77862709e1091750dd5412bef6
shahab <shahabkhan56121@gmail.com>
2021-09-22 19:47:09 +0500

    changes

--- components/Container.tsx
+++ components/Container.tsx
@@ -208,7 +208,7 @@ export default function Container(props: any) {
                   </li>
                 </ul>
                 <a
-                  className="tab-header-item mr-0 setting-icon border-bottom-transparent"
+                  className="tab-header-item mr-0 setting-icon"
                   onClick={() => setOpen(true)}
                 >
                   <Icon settings="settings" />
