5ea5e340ecf08693efad66b4bd142326fbfe6a2d
ilyas <ilyas.datoo@gmail.com>
2021-10-30 06:14:21 +0500

    Changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-import * as wertik from "wertik-js/lib/next";
-import * as express from "express";
+import wertik, { useGraphql } from "wertik-js/lib/next";
 
-const app = express();
-
-console.log(wertik);
-
-app.get("/api", (req, res) => {
-  res.send({ message: "Welcome to api!" });
+wertik({
+  port: process.env.port || 3333,
+  graphql: useGraphql(),
 });
-
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on("error", console.error);
