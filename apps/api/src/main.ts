bc81c112eb0016f1169f12b388b10296f41c1bc9
ilyas <ilyas.datoo@gmail.com>
2021-10-30 05:07:33 +0500

    Importing wrtik

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,17 +2,19 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import * as express from 'express';
+import * as wertik from "wertik-js/lib/next";
+import * as express from "express";
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+console.log(wertik);
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Welcome to api!" });
 });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
