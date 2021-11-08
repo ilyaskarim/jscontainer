9d8f2a475a3e5581f209acf13cde8145500b796f
ilyas <ilyas.datoo@gmail.com>
2021-11-09 04:56:52 +0500

    https redirection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,18 @@ import {
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "normalize.css/normalize.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (
+      window.location.hostname.includes("jscontainer.com") &&
+      window.location.href.startsWith("http:")
+    ) {
+      window.location.href = window.location.href.replace("http:", "https:");
+    }
+  }, []);
+
   return (
     <div className={"bp3-dark"}>
       <QueryClientProvider>
