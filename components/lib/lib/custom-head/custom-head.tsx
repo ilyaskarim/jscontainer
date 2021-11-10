150ca264a4bf6140b064ba4bf407b49640c1cb8f
ilyas <ilyas.datoo@gmail.com>
2021-11-11 01:49:13 +0500

    SEO

--- components/lib/lib/custom-head/custom-head.tsx
+++ components/lib/lib/custom-head/custom-head.tsx
@@ -8,6 +8,19 @@ export const CustomHead = (props: CustomHeadProps) => {
   return (
     <Head>
       <title>{props.title ?? "JS Container"}</title>
+
+      <meta
+        name="description"
+        content="An online code editor to write frontend code using HTML5, Css, Javascript and Typescript."
+      />
+      <meta
+        name="keywords"
+        content="online html format, online css editor, online typescript formatter, online typescript editor, online javascript editor"
+      />
+      <meta name="author" content="JSContaienr" />
+      <meta name="copyright" content="share" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
     </Head>
   );
 };
