87e16a7fd54d1890154445fbb6b752bcaecae9ab
alihussain575 <alihussainirfan575@gmail.com>
2021-12-29 18:24:19 +0500

    created pagination for table

--- components/lib/lib/admin/admin-containers-list/admin-containers-list.tsx
+++ components/lib/lib/admin/admin-containers-list/admin-containers-list.tsx
@@ -1,40 +1,69 @@
-import React, { useEffect, useState } from "react";
-import styles from "./admin-containers-list.module.scss";
-import TableComponent from "../tableComponent/table-component";
-import classnames from "classnames";
-import { useSelector } from "react-redux";
-import { useContainersListQuery } from "../../..";
+import React, { useEffect, useState } from 'react';
+import styles from './admin-containers-list.module.scss';
+import TableComponent from '../tableComponent/table-component';
+import classnames from 'classnames';
+import { useSelector } from 'react-redux';
+import { useContainersListQuery } from '../../..';
+import Pagination from '../pagination/pagination';
 
 /* eslint-disable-next-line */
 export interface AdminContainersListProps {}
 
+const pageData = (data: any[], pageNumber: number) => {
+  return data?.slice((pageNumber - 1) * 5, pageNumber * 5);
+};
+
 export function AdminContainersList(props: AdminContainersListProps) {
   const theme = useSelector((state: any) => state.container.theme);
-  const { data: containersListQueryData } = useContainersListQuery();
+  const { data: containersListQueryData, isLoading } = useContainersListQuery();
   const [data, setData] = useState();
+  const [renderData, setRenderData] = useState<any>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
 
   useEffect(() => {
-    setData(containersListQueryData?.data?.data?.listContainer?.list ?? []);
+    if (!isLoading) {
+      setData(containersListQueryData?.data?.data?.listContainer?.list ?? []);
+      setRenderData(pageData(data, pageNumber));
+      setPageNumber(1);
+    }
   }, [containersListQueryData]);
+
+  useEffect(() => {
+    setRenderData(pageData(data, pageNumber));
+  }, [pageNumber]);
+
+  const handlePagination = (buttonClick: string) => {
+    if (buttonClick === 'decrease') {
+      return setPageNumber(pageNumber - 1);
+    } else if (buttonClick === 'increase') {
+      return setPageNumber(pageNumber + 1);
+    }
+  };
 
   return (
     <div
       className={classnames({
         [styles.editor]: true,
         editor: true,
-        [styles.dark]: theme === "dark",
+        [styles.dark]: theme === 'dark',
       })}
     >
       <h1 className={styles.header}>Containers</h1>
       <TableComponent
         columns={[
-          { title: "Title" },
-          { title: "Description" },
-          { title: "URL" },
-          { title: "Created At" },
-          { title: "Actions(Delete)" },
+          { title: 'Title' },
+          { title: 'Description' },
+          { title: 'URL' },
+          { title: 'Created At' },
+          { title: 'Actions(Delete)' },
         ]}
-        tableData={data}
+        tableData={renderData}
+      />
+      <Pagination
+        handlePagination={handlePagination}
+        data={data}
+        renderData={renderData}
+        pageNumber={pageNumber}
       />
     </div>
   );
--- components/lib/lib/admin/pagination/pagination.tsx
+++ components/lib/lib/admin/pagination/pagination.tsx
@@ -1,10 +1,39 @@
-import styles from "./paginations-styles.module.scss";
+import { Button, Icon } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import styles from './paginations-styles.module.scss';
 
 /* eslint-disable-next-line */
-export interface PaginationProps {}
+export interface PaginationProps {
+  handlePagination?: (buttonClick: string) => void;
+  data?: any[];
+  renderData?: any[];
+  pageNumber?: number;
+}
 
 export function Pagination(props: PaginationProps) {
-  return <div className={styles.paginationContainer}></div>;
+  const { handlePagination, data, renderData, pageNumber } = props;
+
+  return (
+    <div className={styles.paginationContainer}>
+      <p>
+        Showing {renderData?.length} items out of {data?.length}{' '}
+      </p>
+      <div className={styles.paginationButtonStyles}>
+        <Button
+          onClick={() => handlePagination('decrease')}
+          disabled={pageNumber === 1 ? true : false}
+        >
+          <Icon icon='less-than'></Icon>
+        </Button>
+        <Button
+          onClick={() => handlePagination('increase')}
+          disabled={pageNumber === Math.ceil(data?.length / 5) ? true : false}
+        >
+          <Icon icon='greater-than'></Icon>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Pagination;
